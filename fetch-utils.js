581d1c18b5f4f7bc7d5fcc71df66d23ae08f3acc
‘AMAYA <aamaya547@gmail.com>
2022-05-16 11:15:55 -0700

    Updated Supabase URL, Key, and Script

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
-// Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://yhletbviclmzgnzikzpu.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlobGV0YnZpY2xtemduemlrenB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIyOTUzMjIsImV4cCI6MTk2Nzg3MTMyMn0.NSJ1Tw-LlfLsXJF4Y_nS3pwZV2TB7Ru8jvAsLxQLJWg';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
